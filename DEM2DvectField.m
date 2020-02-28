290666845b10367fc606a44be3f60f3fd3f8fb04
Jonathan Jahnke <jahnke@itwm.fraunhofer.de>
2020-02-28 16:25:00 +0100

    Added VectField again

--- DEM2DvectField.m
+++ DEM2DvectField.m
@@ -2,6 +2,6 @@ function F = DEM2DvectField(vx,vz,fx,fz,fwx,fwz,m,par)
     F(1) = vx;
     F(2) = vz;
 
-    F(3) = (sum(fx) + fwx)/m;% + par.g;% - par.g;
-    F(4) = (sum(fz) + fwz)/m- 1.1*par.g;
+    F(3) = (sum(fx) + fwx)/m;% - par.g*0.6;% - par.g;
+    F(4) = (sum(fz) + fwz)/m- par.g;
 end
